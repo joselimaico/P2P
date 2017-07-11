ee0cdde9e9b22ee922d727d5a299b0acaeae8a6a
joselimaico <joselimaico@joselimaico-Satellite-S55t-B>
2017-07-10 19:25:30 -0500

    commit Nodo 1

--- src/com/Peer/Nodo.java
+++ src/com/Peer/Nodo.java
@@ -20,44 +20,61 @@ public class Nodo {
     public static void main (String args[])
     {
       
-      if (args.length < 3) 
+      if (args.length < 2) 
       {
         System.err.println("Usage: java EchoServer <port number>");
         System.exit(1);
       }
     
       int PortS = Integer.parseInt(args[0]);
-      int PortC = Integer.parseInt(args[1]);
-      String hostname = args[2];
+      
+      String hostname = args[1];
      
       boolean listening = true;
       try 
-      (        
-        ServerSocket S = new ServerSocket(PortS);
-        Socket C = new Socket(hostname, PortC); 
-      )
+              
+       
+       // Socket C = new Socket(hostname, PortC);
+              
+      
       {
           String inputline, outputline;
-          while (listening)
-          {
-            //Socket C = new Socket(hostname, PortC); 
-            Socket clienteC = S.accept();
+          //while (listening)
+          //{
+           ServerSocket S = new ServerSocket(PortS);
+          Socket clienteC =S.accept();
+          
+            
+            //C.connect(C.getLocalSocketAddress());
+            
+             
+            
             
             BufferedReader inS = new BufferedReader(new InputStreamReader(clienteC.getInputStream()));
-            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-            PrintWriter outS = new PrintWriter(clienteC.getOutputStream(),true);
+            //PrintWriter outS = new PrintWriter(clienteC.getOutputStream(),true);
+            Socket C = new Socket(hostname, PortS);
             
-            BufferedReader inC = new BufferedReader(new InputStreamReader(C.getInputStream()));
+            
+            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+            
+            
+           // BufferedReader inC = new BufferedReader(new InputStreamReader(C.getInputStream()));
             PrintWriter outC = new PrintWriter(C.getOutputStream(),true);
             
-            while((inputline=stdin.readLine())!=null && inS.readLine()!=null)
+            while((inputline=stdin.readLine())!=null&&(outputline=inS.readLine())!=null)
             {
                 
-                outC.println(inputline);
                 
-                System.out.println(inS.readLine());
+                
+                outC.println(inputline);
+                System.out.println(outputline);
+                
+                
             }
-          }
+            
+            //S.close();
+            //C.close();
+          //}
       } 
         
         catch (Exception e) 
