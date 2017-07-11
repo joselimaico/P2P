e2034eeda51dd90d3e0b35ff87db65a5b76ea6a2
joselimaico <joselimaico@joselimaico-Satellite-S55t-B>
2017-07-10 20:11:45 -0500

    commit Nodo Final

--- src/com/Peer/Nodo.java
+++ src/com/Peer/Nodo.java
@@ -6,6 +6,7 @@
 package com.Peer;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -17,69 +18,58 @@ import java.net.Socket;
  */
 public class Nodo {
  
-    public static void main (String args[])
+     public static void main (String args[]) throws IOException
     {
-      
-      if (args.length < 2) 
-      {
-        System.err.println("Usage: java EchoServer <port number>");
-        System.exit(1);
-      }
-    
-      int PortS = Integer.parseInt(args[0]);
-      
-      String hostname = args[1];
-     
-      boolean listening = true;
-      try 
-              
-       
-       // Socket C = new Socket(hostname, PortC);
-              
-      
-      {
-          String inputline, outputline;
-          //while (listening)
-          //{
-           ServerSocket S = new ServerSocket(PortS);
-          Socket clienteC =S.accept();
-          
-            
-            //C.connect(C.getLocalSocketAddress());
-            
-             
-            
-            
-            BufferedReader inS = new BufferedReader(new InputStreamReader(clienteC.getInputStream()));
-            //PrintWriter outS = new PrintWriter(clienteC.getOutputStream(),true);
-            Socket C = new Socket(hostname, PortS);
-            
-            
-            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-            
-            
-           // BufferedReader inC = new BufferedReader(new InputStreamReader(C.getInputStream()));
-            PrintWriter outC = new PrintWriter(C.getOutputStream(),true);
-            
-            while((inputline=stdin.readLine())!=null&&(outputline=inS.readLine())!=null)
-            {
-                
-                
-                
-                outC.println(inputline);
-                System.out.println(outputline);
-                
-                
-            }
-            
-            //S.close();
-            //C.close();
-          //}
-      } 
-        
-        catch (Exception e) 
+
+        if(args.length < 3)
         {
-            e.getStackTrace();
-       	}
+            System.out.println("Debe ingresar: su puerto, el puerto al que quiere comunicarse y el hostname");
+            System.exit(-1);
+        }
+        
+        int PortS = Integer.parseInt(args[0]);
+        int PortC = Integer.parseInt(args[1]);
+        String hostname = args[2];
+        
+        boolean listening = true;
+        
+        try
+        (
+            ServerSocket S = new ServerSocket(PortS);
+                
+        )
+        {
+                
+            
+            while(listening)
+            {
+                try
+                (
+                    Socket C = new Socket(hostname, PortC);
+                    BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+                    PrintWriter writerC = new PrintWriter(C.getOutputStream(),true);
+                )
+                {
+                    Socket clientS = S.accept();
+                    BufferedReader readerS = new BufferedReader(new InputStreamReader(clientS.getInputStream()));
+                    String input;
+                    
+                    while((input=stdin.readLine())!=null)
+                    {
+                        writerC.println(input);
+                        System.out.println(readerS.readLine());
+                    }
+                    
+                }
+                catch(Exception ex)
+                {
+                    ex.printStackTrace();
+                }                                                    
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
